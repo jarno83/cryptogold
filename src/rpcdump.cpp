13af473b6e1a6876dc959d748ff4e9da9e808f93
jarno83 <jarnoorasmae@hot.ee>
2014-03-12 18:51:22 +0200

    Update rpcdump.cpp

--- src/rpcdump.cpp
+++ src/rpcdump.cpp
@@ -36,7 +36,7 @@ Value importprivkey(const Array& params, bool fHelp)
 {
     if (fHelp || params.size() < 1 || params.size() > 2)
         throw runtime_error(
-            "importprivkey <Zeitcoinprivkey> [label]\n"
+            "importprivkey <cryptogoldprivkey> [label]\n"
             "Adds a private key (as returned by dumpprivkey) to your wallet.");
 
     string strSecret = params[0].get_str();
@@ -75,13 +75,13 @@ Value dumpprivkey(const Array& params, bool fHelp)
 {
     if (fHelp || params.size() != 1)
         throw runtime_error(
-            "dumpprivkey <Zeitcoinaddress>\n"
-            "Reveals the private key corresponding to <Zeitcoinaddress>.");
+            "dumpprivkey <cryptogoldaddress>\n"
+            "Reveals the private key corresponding to <cryptogoldaddress>.");
 
     string strAddress = params[0].get_str();
     CBitcoinAddress address;
     if (!address.SetString(strAddress))
-        throw JSONRPCError(RPC_INVALID_ADDRESS_OR_KEY, "Invalid Zeitcoin address");
+        throw JSONRPCError(RPC_INVALID_ADDRESS_OR_KEY, "Invalid CryptoGold Coin address");
     if (fWalletUnlockMintOnly) // ppcoin: no dumpprivkey in mint-only mode
         throw JSONRPCError(RPC_WALLET_UNLOCK_NEEDED, "Wallet is unlocked for minting only.");
     CKeyID keyID;

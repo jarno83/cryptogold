f47e080cfd6f95a1f8d365a848fd0dc323537f2e
jarno83 <jarnoorasmae@hot.ee>
2014-03-12 18:43:35 +0200

    Update overviewpage.cpp

--- src/qt/overviewpage.cpp
+++ src/qt/overviewpage.cpp
@@ -178,7 +178,7 @@ void OverviewPage::setModel(WalletModel *model)
         connect(model->getOptionsModel(), SIGNAL(displayUnitChanged(int)), this, SLOT(updateDisplayUnit()));
     }
 
-    // update the display unit, to not use the default ("ZEIT")
+    // update the display unit, to not use the default ("CGC")
     updateDisplayUnit();
 }
 

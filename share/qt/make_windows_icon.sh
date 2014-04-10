4dfffb6e65780a73db705e358648fdcfc272ae95
jarno83 <jarnoorasmae@hot.ee>
2014-04-10 16:18:01 +0300

    Update make_windows_icon.sh

--- share/qt/make_windows_icon.sh
+++ share/qt/make_windows_icon.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 # create multiresolution windows icon
-ICON_DST=../../src/qt/res/icons/Zeitcoin.ico
+ICON_DST=../../src/qt/res/icons/cryptogold.ico
 
-convert ../../src/qt/res/icons/Zeitcoin-16.png ../../src/qt/res/icons/Zeitcoin-32.png ../../src/qt/res/icons/Zeitcoin-48.png ${ICON_DST}
+convert ../../src/qt/res/icons/cryptogold-16.png ../../src/qt/res/icons/cryptogold-32.png ../../src/qt/res/icons/cryptogold-48.png ${ICON_DST}
